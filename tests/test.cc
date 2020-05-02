cf7f33b9d2e601051e4166753a77d633f82a819d
Kaahan <kaahan15@gmail.com>
2020-05-02 15:13:20 -0500

    Update tests to use SECTIONS

--- tests/test.cc
+++ tests/test.cc
@@ -14,7 +14,14 @@ using std::string;
 using std::make_pair;
 
 const int kBoardSize = 8;
+const bool is_white_turn = false;
 
+/**
+ *
+ *
+ * @param coord_to_color_map
+ * @return
+ */
 vector<vector<string>> FillGameBoard(const map<pair<int, int>, string>&
     coord_to_color_map) {
   vector<vector<string>> game_board;
@@ -31,6 +38,12 @@ vector<vector<string>> FillGameBoard(const map<pair<int, int>, string>&
   return game_board;
 }
 
+/**
+ *
+ *
+ * @param game_board
+ * @return
+ */
 vector<vector<string>> GetFullBoard(vector<vector<string>>& game_board) {
   // Fills the entire game board with black or white
   for (size_t i = 0; i < kBoardSize; i++) {
@@ -51,31 +64,22 @@ TEST_CASE("Valid moves can be found", "[valid-moves]") {
          {make_pair(3, 4), "black"},
          {make_pair(4, 3), "black"},
          {make_pair(4, 4), "white"}};
-
   vector<vector<string>> game_board = FillGameBoard(coord_to_color_map);
-//  vector<vector<string>> starting_board;
-//  vector<string> v(kBoardSize, "");
-//  for (size_t i = 0; i < kBoardSize; i++) {
-//    starting_board.push_back(v);
-//  }
-//
-//  starting_board[3][3] = "white";
-//  starting_board[3][4] = "black";
-//  starting_board[4][3] = "black";
-//  starting_board[4][4] = "white";
 
-  vector<pair<int, int>> valid_moves;
-  valid_moves.emplace_back(2, 3);
-  valid_moves.emplace_back(3, 2);
-  valid_moves.emplace_back(4, 5);
-  valid_moves.emplace_back(5, 4);
+  vector<pair<int, int>> valid_move_coords;
+  valid_move_coords.emplace_back(2, 3);
+  valid_move_coords.emplace_back(3, 2);
+  valid_move_coords.emplace_back(4, 5);
+  valid_move_coords.emplace_back(5, 4);
 
-  REQUIRE(logic::GetValidMoves(game_board, false) == valid_moves);
+  SECTION("Find valid moves on a normal board") {
+    REQUIRE(logic::GetValidMoves(game_board, false) == valid_move_coords);
+  }
 
-  SECTION("full board") {
+  SECTION("Find valid moves on a full board") {
     game_board = GetFullBoard(game_board);
-    valid_moves.clear();
-    REQUIRE(logic::GetValidMoves(game_board, false) == valid_moves);
+    valid_move_coords.clear();
+    REQUIRE(logic::GetValidMoves(game_board, false) == valid_move_coords);
   }
 }
 
@@ -87,30 +91,36 @@ TEST_CASE("Pieces on the game board can be flipped", "[flip-pieces]") {
      {make_pair(4, 3), "black"},
      {make_pair(4, 4), "white"},
      {make_pair(2, 2), "white"}};
-  vector<vector<string>> starting_board = FillGameBoard(coord_to_color_map);
-//  vector<string> v(kBoardSize, "");
-//  for (size_t i = 0; i < kBoardSize; i++) {
-//    starting_board.push_back(v);
-//  }
-//  starting_board[3][2] = "black";
-//  starting_board[3][3] = "black";
-//  starting_board[3][4] = "black";
-//  starting_board[4][3] = "black";
-//  starting_board[4][4] = "white";
-//  starting_board[2][2] = "white";
-
-  // Creates the game board with the same pieces (before any flips)
-  vector<vector<string>> expected_game_board = starting_board;
-  // This is the piece that should flip to white if the last move was (2, 2)
-  expected_game_board[3][3] = "white";
+  vector<vector<string>> game_board = FillGameBoard(coord_to_color_map);
   int x_move = 2;
   int y_move = 2;
 
-  REQUIRE(logic::FlipPieces(x_move, y_move, true, starting_board)
-          == expected_game_board);
+  SECTION("Flip pieces on a normal board") {
+    // {3, 3} should flip to white if the last move was {2, 2}
+    coord_to_color_map[{3, 3}] = "white";
+    vector<vector<string>> expected_game_board
+      = FillGameBoard(coord_to_color_map);
+
+    REQUIRE(logic::FlipPieces(x_move, y_move, true, game_board)
+            == expected_game_board);
+  }
+
+  SECTION("Flip pieces with an invalid move") {
+    x_move = 7;
+
+    REQUIRE(logic::FlipPieces(x_move, y_move, true, game_board)
+            == game_board);
+  }
+
+  SECTION("Flip pieces on a completely full board") {
+    game_board = GetFullBoard(game_board);
+
+    REQUIRE(logic::FlipPieces(x_move, y_move, is_white_turn, game_board)
+            == game_board);
+  }
 }
 
-TEST_CASE("Is Move Valid", "[is-move-valid]") {
+TEST_CASE("Checks If Moves Are Valid", "[is-move-valid]") {
   map<pair<int, int>, string> coord_to_color_map
       = {{make_pair(3, 3), "white"},
          {make_pair(3, 4), "black"},
@@ -118,17 +128,39 @@ TEST_CASE("Is Move Valid", "[is-move-valid]") {
          {make_pair(4, 4), "white"}};
 
   vector<vector<string>> game_board = FillGameBoard(coord_to_color_map);
-
   int x_move = 2;
   int y_move = 3;
-  REQUIRE(logic::IsMoveValid(x_move, y_move, false, game_board));
-  y_move = 2;
-  REQUIRE(!logic::IsMoveValid(x_move, y_move, false, game_board));
-  game_board = GetFullBoard(game_board);
-  REQUIRE(!logic::IsMoveValid(x_move, y_move, false, game_board));
+
+  SECTION("Test for a Valid Move") {
+    REQUIRE(logic::IsMoveValid(x_move, y_move, is_white_turn, game_board));
+  }
+
+  SECTION("Test for an Invalid Move") {
+    y_move = 2;
+
+    REQUIRE(!logic::IsMoveValid(x_move, y_move, is_white_turn, game_board));
+  }
+
+  SECTION("Test for when the board is completely full") {
+    game_board = GetFullBoard(game_board);
+
+    REQUIRE(!logic::IsMoveValid(x_move, y_move, is_white_turn, game_board));
+  }
 }
 
-TEST_CASE("In bounds", "[in-bounds]") {
-  REQUIRE(logic::InBounds(4, 4)); // Checks for in-bounds coordinates
-  REQUIRE(!logic::InBounds(9, 1)); // Checks for out-of-bounds coordinates
+TEST_CASE("Checks if Coordinates Are In Bounds", "[in-bounds]") {
+  int x_coord = 0;
+  int y_coord = 0;
+
+  SECTION("In Bounds Coordinates") {
+    // Checks for in-bounds coordinates (x or y less than or equal to 7)
+    REQUIRE(logic::InBounds(x_coord, y_coord));
+  }
+
+  SECTION("Out of Bounds Coordinates") {
+    y_coord = 10;
+    
+    // Checks for out-of-bounds coordinates (x or y greater than 7)
+    REQUIRE(!logic::InBounds(x_coord, y_coord));
+  }
 }
